513f43127cd1badf594ce4aa6ed99fe84b3f8089
claudiutucmeanu <claudiutucmeanu@gmail.com>
2024-08-02 16:47:44 +0300

    added header component, news page

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import { Typography } from "@mui/material";
+import "./Header.scss";
+
+function Header() {
+	return (
+		<Typography variant='h1' fontWeight={200}>
+			NEWS
+		</Typography>
+	);
+}
+
+export default Header;
---
+++ src/news/NewsPage.tsx
@@ -0,0 +1,7 @@
+import { Typography } from "@mui/material";
+
+function NewsPage() {
+	return <Typography variant='h1'>newspage</Typography>;
+}
+
+export default NewsPage;
